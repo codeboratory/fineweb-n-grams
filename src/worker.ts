07509e59af74c7b508b503aed327a10f3d760300
Yamiteru <yamiteru@icloud.com>
2025-05-01 22:51:15 +0200

    feat: add worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,93 +1,79 @@
+import { DuckDBInstance, DuckDBVarCharVector } from "@duckdb/node-api";
+import bindings from "@duckdb/node-bindings";
 import process from "node:process";
+import type { Data } from "./types";
+import { CHUNK_SIZE } from "./constants";
+import { LanguageDB } from "./database/language";
+import { bigrams_map, computeGrams, trigrams_map, unigrams_map } from "./ngrams";
+import { getRandomSleepDuration, sleep } from "./utils";
 
-export const workerProcess = async () => {
-	process.on("message", (data) => {
-		console.log("Data from main: ", data);
+const text_decoder = new TextDecoder();
+
+const duck_instance = await DuckDBInstance.create();
+const duck_connection = await duck_instance.connect();
+
+const read_query = await duck_connection.prepare(
+	"SELECT text FROM read_parquet($1) LIMIT $2 OFFSET $3;"
+);
+
+// // TODO: has to be mutable
+const language_db = new LanguageDB("eng");
+
+export const workerProcess = () => {
+	process.on("message", async (data) => {
+		console.log("New job", data);
+
+		const input = data as Data;
+
+		await sleep(getRandomSleepDuration());
+
+		read_query.bindVarchar(1, `data/${input.directory}/${input.file}`);
+		read_query.bindInteger(2, CHUNK_SIZE);
+		read_query.bindInteger(3, input.chunk * CHUNK_SIZE);
+
+		const data_reader = await read_query.stream();
+
+		while (true) {
+			const chunk = await data_reader.fetchChunk();
+
+			if (!chunk || chunk.rowCount === 0) {
+				break;
+			}
+
+			const text_vector = chunk.getColumnVector(0) as DuckDBVarCharVector;
+			const data_view = (text_vector as unknown as { dataView: DataView }).dataView;
+			const buffer = data_view.buffer as ArrayBuffer;
+
+			for (let i = 0; i < text_vector.itemCount; ++i) {
+				computeGrams(
+					text_decoder.decode(
+						bindings.get_data_from_pointer(
+							buffer,
+							data_view.byteOffset + (i * 16) + 8,
+							data_view.getUint32(i * 16, true)
+						)
+					)
+				);
+			}
+		}
+
+		// TODO: get info if the file is done and
+		// if it is then remove the file from /data
+		// and mark the file in database as done
+		// and check if directory is also done
+		// and check if language is also done
+		await language_db.insertOneChunk(input.directory, input.file, input.chunk, {
+			unigrams: unigrams_map,
+			bigrams: bigrams_map,
+			trigrams: trigrams_map
+		});
+
+		unigrams_map.clear();
+		bigrams_map.clear();
+		trigrams_map.clear();
+
+		process.send?.(data);
 	});
 
 	process.send?.(true);
-
-	// console.log("THREAD Worker");
-	//
-	// const text_decoder = new TextDecoder();
-	//
-	// const instance = await DuckDBInstance.create(':memory:', {
-	//   threads: WORKER_COUNT
-	// });
-	//
-	// const duckdb = await instance.connect();
-	// const read_query = await duckdb.prepare("SELECT text FROM read_parquet($1) LIMIT $2 OFFSET $3;");
-	//
-	// // TODO: has to be mutable
-	// const database = openDatabase("english");
-	//
-	// const files_db = database.openDB({ name: "files" });
-	// const unigrams_db = database.openDB({ name: "unigrams" });
-	// const bigrams_db = database.openDB({ name: "bigrams" });
-	// const trigrams_db = database.openDB({ name: "trigrams" });
-	//
-	// process.on("message", async (event) => {
-	// 	console.log("Main message", event);
-	//
-	// 	const input = event as Input;
-	//
-	// 	read_query.bindVarchar(1, `data/${input.dir}/${input.file}`);
-	// 	read_query.bindInteger(2, input.limit);
-	// 	read_query.bindInteger(3, input.offset);
-	//
-	// 	const data_reader = await read_query.stream();
-	//
-	// 	while(true) {
-	// 		unigrams_map.clear();
-	// 		bigrams_map.clear();
-	// 		trigrams_map.clear();
-	//
-	// 		const chunk = await data_reader.fetchChunk();
-	//
-	// 		if (!chunk || chunk.rowCount === 0) {
-	// 			break;
-	// 		}
-	//
-	// 		const text_vector = chunk.getColumnVector(0) as DuckDBVarCharVector;
-	// 		const data_view = text_vector.dataView as DataView;
-	// 		const buffer = data_view.buffer as ArrayBuffer;
-	//
-	// 		for (let i = 0; i < text_vector.itemCount; ++i) {
-	// 			computeGrams(
-	// 				text_decoder.decode(
-	// 					bindings.get_data_from_pointer(
-	// 						buffer,
-	// 						data_view.byteOffset + (i * 16) + 8,
-	// 						data_view.getUint32(i * 16, true)
-	// 					)
-	// 				)
-	// 			);
-	// 		}
-	//
-	// 		await unigrams_db.transaction(() => {
-	// 			const file_value = files_db.get(input.file) as FileValue;
-	//
-	// 			file_value.chunks[input.index] = true;
-	// 			file_value.done = file_value.chunks.includes(false);
-	//
-	// 			files_db.put(input.file, file_value);
-	//
-	// 			for (const [key, value] of unigrams_map) {
-	// 				unigrams_db.put(key, (unigrams_db.get(key) ?? 0) + value);
-	// 			}
-	//
-	// 			for (const [key, value] of bigrams_map) {
-	// 				bigrams_db.put(key, (bigrams_db.get(key) ?? 0) + value);
-	// 			}
-	//
-	// 			for (const [key, value] of trigrams_map) {
-	// 				trigrams_db.put(key, (trigrams_db.get(key) ?? 0) + value);
-	// 			}
-	// 		});
-	// 	}
-	//
-	// 	process.send(State.DONE);
-	// });
-	//
-	// process.send(State.READY);
 };
